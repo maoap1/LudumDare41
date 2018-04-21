b0148ecf41e686dc686236aaacfe82acbbee1e98
maoap1 <maoap1@gmail.com>
2018-04-21 18:18:41 +0200

    other staff

--- LudumDare41/Assets/Scripts/PlayerController.cs
+++ LudumDare41/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,29 @@
-﻿using System.Collections;
+﻿
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerController : MonoBehaviour
 {
+    const int ID_RIGHT = 0;
+    const int ID_UP = 1;
+    const int ID_DOWN = 2;
+    const int ID_LEFT = 3;
+
 
     public int tileSize = 100;
     public bool DEBUG = false;
 
+    public bool move = false;
+
     public float fireDelta = 0.5F;
     private float nextFire = 0.5F;
     private float myTime = 0.0F;
+   
 
-    private bool press = false;
+    //private bool press = false;
     public GameObject pointPre;
     public List<GameObject> points = new List<GameObject>();
     Vector3 pozice;
@@ -28,11 +39,17 @@ public class PlayerController : MonoBehaviour
 
         if (Input.GetButtonDown("Right"))// && myTime > nextFire)
         {
+
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector3.right);
+            hit.collider.gameObject.GetComponent<WallController>().lives--;
+            //GameObject probe = GameObject.Find("Probe");
+            //probe.transform.Translate(Vector3.right * tileSize);
+
             nextFire = myTime + fireDelta;
-            transform.Translate(Vector3.right * tileSize);
+            //transform.Translate(Vector3.right * tileSize);
             nextFire = nextFire - myTime;
             myTime = 0.0F;
-            press = true;
+            //press = true;
         }
         else if (Input.GetButtonDown("Down"))// && myTime > nextFire)
         {
@@ -40,7 +57,7 @@ public class PlayerController : MonoBehaviour
             transform.Translate(Vector3.down * tileSize);
             nextFire = nextFire - myTime;
             myTime = 0.0F;
-            press = true;
+            //press = true;
         }
         else if (Input.GetButtonDown("Up"))// && myTime > nextFire)
         {
@@ -48,7 +65,7 @@ public class PlayerController : MonoBehaviour
             transform.Translate(Vector3.up * tileSize);
             nextFire = nextFire - myTime;
             myTime = 0.0F;
-            press = true;
+            //press = true;
         }
 
 
@@ -58,16 +75,17 @@ public class PlayerController : MonoBehaviour
             transform.Translate(Vector3.left * tileSize);
             nextFire = nextFire - myTime;
             myTime = 0.0F;
-            press = true;
+            //press = true;
         }
-
-        if (press)
+        
+        if (move)
         {
+
             pozice = transform.position;
             pozice.z = -5;
             GameObject point = (GameObject)Instantiate(pointPre, pozice, transform.rotation);
             points.Add(point);
-            press = false;
+            move = false;
         }
     }
     
--- LudumDare41/Assets/Shaders and Materials/Water.shader
+++ LudumDare41/Assets/Shaders and Materials/Water.shader
@@ -2,21 +2,28 @@
 {
 	Properties
 	{
-		_NoiseTex0("NoiseTexture0", 2D) = "white" {}
-		_NoiseTex1("NoiseTexture1", 2D) = "black" {}
+		_NoiseTex0 ("NoiseTexture", 2D) = "white" {}
+		_NoiseTex1("DistortionTexture", 2D) = "black" {}
+		_Color0 ("Color0", Color) = (0.0, 0.0, 0.0, 1.0)
+        _Color1 ("Color1", Color) = (1.0, 1.0, 1.0, 1.0)
 
-		_Color0("Color0", Color) = (0.0, 0.0, 0.0, 1.0)
-		_Color1("Color1", Color) = (1.0, 1.0, 1.0, 1.0)
+        _DistortionStrength ("Distortion Strength", Range(0.0, 2.0)) = 1.0
+        _Speed ("Speed", Float) = 1.0
 	}
 	SubShader
 	{
-		Tags { "RenderType"="Opaque" }
-
+		Tags { "Queue"="Transparent" "RenderType"="Transparent" }
 		Pass
 		{
+
+		
+            ZWrite Off
+            Cull Off
+
 			CGPROGRAM
 			#pragma vertex vert
 			#pragma fragment frag
+			
 			#include "UnityCG.cginc"
 
 			struct appdata
@@ -28,29 +35,38 @@
 			struct v2f
 			{
 				float2 uv : TEXCOORD0;
-				UNITY_FOG_COORDS(1)
 				float4 vertex : SV_POSITION;
+       		 	float2 uv0 : TEXCOORD1;
+        		float2 uv1 : TEXCOORD2;
 			};
 
-			sampler2D _NoiseTex0;
-			float4 _NoiseTex0_ST;
 			sampler2D _NoiseTex1;
-			float4 _NoiseTex1_ST;
-			fixed4 _Color0;
-			fixed4 _Color1;
+    		half4 _NoiseTex1_ST;
+			sampler2D _NoiseTex0;
+			half4 _NoiseTex0_ST;
+			fixed4 _Color0;    
+    		fixed4 _Color1; 
+    		half _DistortionStrength;
+    		float _Speed;
 			
 			v2f vert (appdata v)
 			{
 				v2f o;
 				o.vertex = UnityObjectToClipPos(v.vertex);
-				o.uv = TRANSFORM_TEX(v.uv, _NoiseTex1);
+				o.uv = v.uv;
+				o.uv0 = TRANSFORM_TEX(v.uv, _NoiseTex0);
+       			o.uv1 = TRANSFORM_TEX(v.uv, _NoiseTex1);
 				return o;
 			}
 			
 			fixed4 frag (v2f i) : SV_Target
 			{
-				fixed4 col = tex2D(_NoiseTex1, i.uv);
-				return col;
+        		float2 dist = (tex2D(_NoiseTex1, i.uv1).xy - float2(0.5, 0.5))*_DistortionStrength;
+        		float m = tex2D(_NoiseTex0, i.uv0 + dist - float2(_Time.x * _Speed*2, _Time.x * _Speed)).r;
+				float t = m + (1.0 - i.uv.y);
+				fixed4 c = lerp(_Color1, _Color0, i.uv.y) * t;
+       			c.a = t;
+        		return c;
 			}
 			ENDCG
 		}

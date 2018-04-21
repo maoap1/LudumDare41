569db68b9e328fcc3880ce354e551979e8fb3490
Tomáš Jakubec <samot.cebukaj@seznam.cz>
2018-04-21 11:02:33 +0200

    samé ryby

---
+++ LudumDare41/Assets/Spawn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public GameObject fish;
+
+    public List<GameObject> fishes = new List<GameObject>(); 
+
+    public bool Spawning = false;
+
+	// Update is called once per frame
+	void Update () {
+		if (Spawning)
+        {
+            Vector3 pozice = new Vector3(-650, -250, -5);
+            GameObject spawnfish = (GameObject)Instantiate(fish, pozice, transform.rotation);
+            fishes.Add(spawnfish);
+            Spawning = false;
+        }
+	}
+}

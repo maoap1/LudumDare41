0281978e2cb3992857f99d39b8fd86c837d57981
maoap1 <maoap1@gmail.com>
2018-04-21 11:01:33 +0200

    PlayerController

---
+++ LudumDare41/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public int tileSize = 100;
+    public bool DEBUG = false;
+
+    public float fireDelta = 0.5F;
+    private float nextFire = 0.5F;
+    private float myTime = 0.0F;
+
+    void Start ()
+    {
+		
+	}
+
+    void Update()
+    {
+        myTime = myTime + Time.deltaTime;
+
+        if (Input.GetButtonDown("Right"))// && myTime > nextFire)
+        {
+            nextFire = myTime + fireDelta;
+            transform.Translate(Vector3.right * tileSize);
+            nextFire = nextFire - myTime;
+            myTime = 0.0F;
+        }
+        else if (Input.GetButtonDown("Down"))// && myTime > nextFire)
+        {
+            nextFire = myTime + fireDelta;
+            transform.Translate(Vector3.down * tileSize);
+            nextFire = nextFire - myTime;
+            myTime = 0.0F;
+        }
+        else if (Input.GetButtonDown("Up"))// && myTime > nextFire)
+        {
+            nextFire = myTime + fireDelta;
+            transform.Translate(Vector3.up * tileSize);
+            nextFire = nextFire - myTime;
+            myTime = 0.0F;
+        }
+
+
+        else if (Input.GetButtonDown("Left") && DEBUG)// && myTime > nextFire)
+        {
+            nextFire = myTime + fireDelta;
+            transform.Translate(Vector3.left * tileSize);
+            nextFire = nextFire - myTime;
+            myTime = 0.0F;
+        }
+    }
+    
+}
